f58a63c102471c7f27965cc99953db9339fcb6aa
Ryleigh <78240770+ryleigh-stefanski@users.noreply.github.com>
2024-06-05 22:33:14 -0400

    Made it so that ascends scraper returns price, brand, and size

--- Indica_Scrapper_01.py
+++ Indica_Scrapper_01.py
@@ -2,30 +2,50 @@ import selenium_ascend as ascend
 import selenium_ethos as ethos
 import time
 
+line = "--------------------------------------------------------"
+
 #urls
 ascend_url = "https://letsascend.com/menu/pa-scranton-menu-med/categories/flower"
 ethos_url = "https://wilkesbarre.ethoscannabis.com/stores/ethos-wilkes-barre/products/flower"
 
-search_key = "Gello"
+ascend_found = 0
+ascend_price = 0
+ascend_brand = 0
+ascend_size = 0
+
+ethos_found = 0
+ethos_price = 0
+ethos_brand = 0
+ethos_size = 0
+
+search_key = "Alien OG"
 
 max_scrolls_to_bottom = 30
 max_elements_checked = 300
-wait_for_load_time = 0.15
+wait_for_load_time = 0.005
 
 found_at_ascend = False
 found_at_ethos = False
 
 try:
-    found_at_ascend = ascend.sel_ascend_run(search_key, ascend_url, max_scrolls_to_bottom,max_elements_checked,wait_for_load_time)
+    ascend_found, ascend_price, ascend_size, ascend_brand = ascend.sel_ascend_run(search_key, ascend_url, max_scrolls_to_bottom,max_elements_checked,wait_for_load_time)
 except:
     print("Failed to search Ascend...")
     
 try:
-    found_at_ethos = ethos.sel_ethos_run(search_key, ethos_url)
+    ethos_found = ethos.sel_ethos_run(search_key, ethos_url)
 except:
     print("Failed to search Ethos...")
 
-print("------------------------------------------------------------------------")
-print("In stock at Ascend: %s" % found_at_ascend)
-print("In stock at Ethos: %s" % found_at_ethos)
-print("------------------------------------------------------------------------")
+
+dots_per_side = int((len(line) - len(search_key)) / 2) * "."
+print("\n\n%sResults for: %s%s" %  (dots_per_side, search_key, dots_per_side))
+print(line)
+print("In stock at Ascend: %s" % ascend_found)
+if ascend_found:
+    print(".......Price: %s" % ascend_price)
+    print(".......Brand: %s" % ascend_brand)
+    print(".......Size : %s" % ascend_size)
+
+print("\nIn stock at Ethos: %s" % ethos_found)
+print(line)
--- selenium_ascend.py
+++ selenium_ascend.py
@@ -5,17 +5,23 @@ import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import re
 
 #return i = -1 if element found
+#if found returns, price, size, and brand in that order
 def check_for_element(driver, search_key, max_elements_checked):
-    items = driver.find_elements(By.CSS_SELECTOR, '[class="sc-35f5af31-0 eGRdab"]')
+    items = driver.find_elements(By.CSS_SELECTOR, '[class="sc-6ef47149-0 hVgJPP"]')
     num = 0;
     for i in items:
         print("Checking %s / %s items..." % (num, len(items)))
         ele = i.get_attribute("innerHTML")
         if(ele.find(search_key) != -1):
             print("Found at %s" % num)
-            return True
+            price = re.findall(r"\$[0-9]?[0-9]?[0-9]\.?[0-9]?[0-9]?", ele)[0]
+            size = re.findall(r"[0-9]?[0-9].?[0-9][0-9]?g", ele)[0]
+            brand = re.findall(r'<div class="sc-35f5af31-0 cduLoD">(.+)' ,ele)[0]
+            brand = brand[0:brand.find('</div>')] #cuts at first div marker
+            return True, price, size, brand
         else:
             num += 1
     return False
@@ -47,12 +53,13 @@ def open_web_driver(url):
         print("Failed")
     return driver
 
+#if found returns {found status}, price, size, and brand
 def sel_ascend_run(search_key, url, max_scrolls_to_bottom,max_elements_checked,wait_for_load_time):
     driver = open_web_driver(url)
     bypass_age_check(driver)
     traverse_page(driver, max_scrolls_to_bottom, wait_for_load_time)
-    found = check_for_element(driver, search_key, max_elements_checked)	
+    found, price, size, brand = check_for_element(driver, search_key, max_elements_checked)	
     driver.close()
-    return found
+    return found, price, size, brand
     
-#sel_ascend_run("Kitchen Sink", "https://letsascend.com/menu/pa-scranton-menu-med/categories/flower", 30, 300, .15)
+#sel_ascend_run("Alien OG", "https://letsascend.com/menu/pa-scranton-menu-med/categories/flower", 30, 300, .05)
