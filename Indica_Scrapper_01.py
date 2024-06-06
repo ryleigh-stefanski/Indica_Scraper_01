ff8521bc3f1eb53ad1262a1a4de6aefe64ec2211
Ryleigh <78240770+ryleigh-stefanski@users.noreply.github.com>
2024-06-06 14:47:38 -0400

    Add files via upload

--- Indica_Scrapper_01.py
+++ Indica_Scrapper_01.py
@@ -11,12 +11,9 @@ ethos_url = "https://wilkesbarre.ethoscannabis.com/stores/ethos-wilkes-barre/pro
 #returned item object looks like [name, price, size, brand]
 ascend_returned_items = []
 
-ethos_found = 0
-ethos_price = 0
-ethos_brand = 0
-ethos_size = 0
+ethos_returned_items = []
 
-search_key = ["Cream", "Alien OG", "P Punch"]
+search_key = ["Tahoe Apple", "Sled Dog", "Perfect Cell", "CannaConfusion"]
 
 max_scrolls_to_bottom = 30
 max_elements_checked = 300
@@ -25,23 +22,32 @@ wait_for_load_time = 0.1
 found_at_ascend = False
 found_at_ethos = False
 
+#returns the length of all items in a list
+def depth_len(list):
+    length = 0
+    for ele in list:
+        length += len(ele)
+    return length
+
+#search sites  
 try:
     ascend_returned_items = ascend.sel_ascend_run(search_key, ascend_url, max_scrolls_to_bottom,max_elements_checked,wait_for_load_time)
 except:
     print("Failed to search Ascend...")
-    
+   
 try:
-    ethos_found = ethos.sel_ethos_run(search_key[0], ethos_url)
+    ethos_returned_items = ethos.sel_ethos_run(search_key, ethos_url)
 except:
     print("Failed to search Ethos...")
 
 
 #output results
-dots_per_side = int((len(line) - 12 - len(search_key)) / 2) * "."
+dots_per_side = int((len(line) - 6 - depth_len(search_key)) / 2) * "."
 print("\n\n%sResults for: %s%s" %  (dots_per_side, search_key, dots_per_side))
 print(line)
 #first layer of list has the strains i.e. l[0] = cream, l[1] = alien og
 #second layer has information on each items of that strain  i.e. l[0][0] = first cream item, l[1][1] = second alien og item
+print("\n\n%sAscend%s" % (dots_per_side, dots_per_side))
 i = 0
 for strain in ascend_returned_items:
     for element in ascend_returned_items[i]:
@@ -50,6 +56,15 @@ for strain in ascend_returned_items:
         print(".......Brand: %s" % element[2])
         print(".......Size : %s" % element[1])
     i += 1
+
+print("\n\n%sEthos%s" % (dots_per_side, dots_per_side))
+i = 0
+for page in ethos_returned_items:
+    for element in ethos_returned_items[i]:
+        print("....Strain: %s" % element[0])
+        print(".......Price: %s" % element[1][0])
+        print(".......Brand: %s" % element[1][2])
+        print(".......Size : %s" % element[1][1])
+    i += 1
     
-print("\nIn stock at Ethos: %s" % ethos_found)
 print(line)
--- selenium_ethos.py
+++ selenium_ethos.py
@@ -1,31 +1,62 @@
 import web_scraping_selenium_functions as funcs
 
+import re
 import time
 import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def search_page(driver, search_key):   
+#extract price, size, and brand
+def extract_element_values(ele):
+    pr, si, br = "N/A"
+    price = re.findall(r"\$[0-9]?[0-9]?[0-9]\.?[0-9]?[0-9]?", ele)
+    if price:
+        pr = price[0]
+        
+    size = re.findall(r"[1-9]/[1-9] oz", ele)
+    if size:
+        si = size[0]
+        
+   # brand = re.findall(r'<div class="sc-35f5af31-0 cduLoD">(.+)' ,ele)
+    #if brand:
+     #   br = brand[0][0:brand[0].find('</div>')] #cuts at first div marker
+    
+    return pr, si, br
+
+#searches the items list for all items in the search_key list
+def search_page_elements(search_key, items):
+    found_items = []
+    pos = 0
+    for key in search_key:
+        for i in items:
+            print("Checking %s / %s items on this page for key %s..." % (pos, len(items), key))
+            ele = i.get_attribute("innerHTML")
+            if(ele.find(key) != -1):
+                print("Found")
+                found_items.append((key, extract_element_values(ele)))
+            else:
+               pos += 1
+    return found_items
+    
+def search_page(driver, search_key): 
     funcs.scroll_to_bottom(driver, 1)
         
     #check for element
-    items = driver.find_elements(By.CSS_SELECTOR, '[class="desktop-product-list-item__ProductName-sc-8wto4u-7 kjymBK"]')
-
-    pos = 0
-    for i in items:
-        print("Checking %s / %s items on this page..." % (pos, len(items)))
-        ele = i.get_attribute("innerHTML")
-        if(ele.find(search_key) != -1):
-            print("Found")
-            return True
-        else:
-           pos += 1
-    return False
+    try:
+        items = driver.find_elements(By.CSS_SELECTOR, '[class="desktop-product-list-item__Container-sc-8wto4u-0 jTzrhU"]')
+    except:
+        print("Could not retreive elements...")
+        return []
+    return search_page_elements(search_key, items)
 
 def next_page(driver):
     print("Moving to next page...")
-    driver.find_element(By.CSS_SELECTOR, '[class="pagination-controls__NavButton-sc-1436mnk-1 hjQwsb"]').click()
+    try:
+        driver.find_element(By.CSS_SELECTOR, '[class="pagination-controls__NavButton-sc-1436mnk-1 hjQwsb"]').click()
+    except:
+        print("Could not move to next page...")
+        return -1
     time.sleep(1)
 
 #selenium bypass age check
@@ -46,16 +77,24 @@ def bypass_age_check(driver):
     except:
         print("Failed to bypass age check...")
 
+#returns items as such
+#found_items[i] where i = page number
+#found_items[0][j] where j is the element on that certain page
+#found_items(name, [0][0][0-3])where 0 = strain name, 1= price, 2= size, 3 = brand
 def traverse_pages(driver, search_key):
-    found = False
+    found_items = []
     page_number = 1
-    while(found == False):
-        found = search_page(driver, search_key)
-        next_page(driver)
+    while True:
+        found = (search_page(driver, search_key))
+        if found:
+            found_items.append(found)
+        stop = next_page(driver)
+        if stop == -1:
+            return found_items
         page_number += 1
         if page_number > 5:
-            return False  
-    return True
+            break 
+    return found_items
 
 def sel_ethos_run(search_key, url):    
     driver = open_web_driver(url)
@@ -64,4 +103,4 @@ def sel_ethos_run(search_key, url):
     driver.close()
     return found
 
-#sel_ethos_run("Gello", "https://wilkesbarre.ethoscannabis.com/stores/ethos-wilkes-barre/products/flower")
+#sel_ethos_run(["Tahoe Apple", "Sled Dog", "Perfect Cell", "CannaConfusion"], "https://wilkesbarre.ethoscannabis.com/stores/ethos-wilkes-barre/products/flower")
